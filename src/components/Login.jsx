205b5b8e28ede7f62ba1c915fd821e06acabaa1a
Nguyen Quang Anh <youremail@example.com>
2026-07-16 09:25:25 +0700

    Add Facebook, X, and Twitch social login buttons to Login and Signup views

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -145,7 +145,7 @@ const Login = () => {
             <span>OR CONTINUE WITH</span>
           </div>
 
-          {/* Three social buttons side-by-side (Google, Facebook, Apple) */}
+          {/* Five social buttons side-by-side (Google, Facebook, Apple, X, Twitch) */}
           <div className="social-grid">
             <button className="btn-social-auth" type="button" title="Google">
               <svg className="social-svg" viewBox="0 0 24 24">
@@ -163,6 +163,16 @@ const Login = () => {
             <button className="btn-social-auth" type="button" title="Apple">
               <svg className="social-svg" viewBox="0 0 24 24" fill="#ffffff">
                 <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 4.17c.66-.81 1.11-1.93.99-3.06-1 .04-2.22.67-2.94 1.5-.64.74-1.2 1.88-1.05 2.99 1.12.09 2.27-.58 3-.143z"/>
+              </svg>
+            </button>
+            <button className="btn-social-auth" type="button" title="X (Twitter)">
+              <svg className="social-svg" viewBox="0 0 24 24" fill="#ffffff">
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+              </svg>
+            </button>
+            <button className="btn-social-auth" type="button" title="Twitch">
+              <svg className="social-svg" viewBox="0 0 24 24" fill="#9146FF">
+                <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"/>
               </svg>
             </button>
           </div>
